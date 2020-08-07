066b01d8557c25d1d15b2f47c2191721f988527b
amir bekhit <bekdevelopment@gmail.com>
2020-08-07 07:28:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const hr = document.querySelector("#hr");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+
+setInterval(function () {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  min.style.transform = `rotateZ(${mm}deg)`;
+  sec.style.transform = `rotateZ(${ss}deg)`;
+});
